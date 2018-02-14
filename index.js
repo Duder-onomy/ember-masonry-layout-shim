a10b1797c8475317bd285ecf8e15a1d4a167bb70
Greg Larrenaga <greg@thedyrt.com>
2018-02-14 14:05:33 -0800

    WIP

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const Funnel = require('broccoli-funnel');
+const fastbootTransform = require('fastboot-transform');
+const mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-masonry'
+  name: 'ember-masonry',
+
+  treeForVendor(defaultTree) {
+    let masonryJs = fastbootTransform(new Funnel(require.resolve('masonry-layout')), {
+      files: ['masonry.js'],
+      destDir: 'masonry'
+    });
+
+    return mergeTrees([defaultTree, masonryJs]);
+  },
+
+  included() {
+    this._super(arguments);
+
+    this.import('vendor/masonry/masonry.js');
+  },
 };
