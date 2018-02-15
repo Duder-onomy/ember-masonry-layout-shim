fbeab556670acf0260502e20badee5067fc1a9a5
Greg Larrenaga <greg@thedyrt.com>
2018-02-14 16:51:36 -0800

    WIP

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 'use strict';
 
-const Funnel = require('broccoli-funnel');
-const fastbootTransform = require('fastboot-transform');
 const mergeTrees = require('broccoli-merge-trees');
+const path = require('path');
+const funnel = require('broccoli-funnel');
+const UnwatchedDir = require('broccoli-source').UnwatchedDir;
+const map = require('broccoli-stew').map;
+const resolve = require('resolve');
 
 module.exports = {
   name: 'ember-masonry',
 
-  treeForVendor(defaultTree) {
-    let masonryJs = fastbootTransform(new Funnel(require.resolve('masonry-layout')), {
-      files: ['masonry.js'],
-      destDir: 'masonry'
-    });
+  treeForVendor(vendorTree) {
+    let trees = [];
 
-    return mergeTrees([defaultTree, masonryJs]);
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(
+      funnel(this.masonryJs, {
+        destDir: 'masonry',
+        include: [new RegExp(/\.js$/)],
+        exclude: ['tests', 'ender', 'package'].map(
+          key => new RegExp(key + '.js$')
+        )
+      })
+    );
+
+    return map(
+      mergeTrees(trees),
+      content => `if (typeof FastBoot === 'undefined') { ${content} }`
+    );
   },
 
   included() {
     this._super(arguments);
-
+    this.masonryJs = new UnwatchedDir(path.dirname(
+      resolve.sync('masonry', { basedir: this.project.root })
+    ));
     this.import('vendor/masonry/masonry.js');
   },
 };
